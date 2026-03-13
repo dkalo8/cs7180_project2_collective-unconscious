dcb5095c5e67f238adfdb13c3067c5d68ffff744
Ash <chen.zhanyi@northeastern.edu>
2026-03-13 00:45:10 -0400

    fix: resolve linting errors in vitest.setup.js

--- client/vitest.setup.js
+++ client/vitest.setup.js
@@ -1,3 +1,4 @@
+/* global global */
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 
@@ -10,7 +11,7 @@ vi.stubEnv('VITE_API_URL', 'http://127.0.0.1:3000');
 
 // Mock ResizeObserver for components that use it (like ShareModal)
 global.ResizeObserver = class {
-  constructor(callback) {}
+  constructor() {}
   disconnect() {}
   observe() {}
   unobserve() {}
